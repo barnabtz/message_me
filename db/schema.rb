e09c2482df6df21745f0fe242c7eecd6d9e12e27
Fon Barnabas Afuoh <barnabtz@gmail.com>
2020-08-13 20:44:26 +0100

    authentication and messages setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_182731) do
+ActiveRecord::Schema.define(version: 2020_08_13_192737) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "barnabtz", password: "Westics02")
+User.create(username: "darrylfon0", password: "Westics02+")
+User.create(username: "bekinaj", password: "Westics02+")
+User.create(username: "shalin00", password: "Westics02+")
